4bc1659995d6c9d04fafa2e91992efea2cad489a
Binay <binaydas1123@gmail.com>
2026-01-15 18:47:23 +0530

    auth middleware

---
+++ proxy.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+    function middleware(req) {
+        return NextResponse.next()
+    },
+    {
+        callbacks: {
+            authorized: ({ req, token }) => {
+                const path = req.nextUrl.pathname
+
+                if (path === '/') {
+                    return true
+                }
+
+                return !!token
+            },
+        },
+        pages: {
+            signIn: '/signin',
+        },
+    }
+)
+
+export const config = {
+    matcher: [
+        "/((?!api/auth|api/register|signin|signup|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$|.*\\.webp$).*)",
+    ]
+}
