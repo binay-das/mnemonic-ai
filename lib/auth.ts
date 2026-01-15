28ac2392dcbb8e3db957c6f99b69df2ad43f4d35
Binay <binaydas1123@gmail.com>
2026-01-15 18:42:07 +0530

    auth middleware

---
+++ lib/auth.ts
@@ -0,0 +1,68 @@
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "user@example.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    return null;
+                }
+
+                try {
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            email: credentials.email
+                        }
+                    });
+
+                    if (!user) {
+                        return null;
+                    }
+
+                    const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+
+                    if (!isPasswordValid) {
+                        return null;
+                    }
+
+                    return {
+                        id: user.id,
+                        email: user.email,
+                        name: user.name
+                    };
+                } catch (error) {
+                    return null;
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    },
+    pages: {
+        signIn: '/signin'
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                (session.user as any).id = token.id;
+            }
+            return session;
+        }
+    }
+}
