8dfb35f0197117e96b47f1671d2f7e527da41857
Binay <binaydas1123@gmail.com>
2026-01-16 08:07:29 +0530

    feat: function for finding cosine similaroty between 2 vectors

--- lib/embeddings.ts
+++ lib/embeddings.ts
@@ -33,3 +33,27 @@ export async function generateEmbeddings(
     return embeddings;
 }
 
+export function cosineSimilarity(vecA: number[], vecB: number[]): number {
+    if (vecA.length !== vecB.length) {
+        throw new Error('Embeddings must have the same dimension');
+    }
+
+    let dotProduct = 0;
+    let mA = 0;
+    let mB = 0;
+
+    for (let i = 0; i < vecA.length; i++) {
+        dotProduct += vecA[i] * vecB[i];
+        mA += vecA[i] * vecA[i];
+        mB += vecB[i] * vecB[i];
+    }
+
+    mA = Math.sqrt(mA);
+    mB = Math.sqrt(mB);
+
+    if (mA === 0 || mB === 0) {
+        return 0;
+    }
+
+    return dotProduct / (mA * mB);
+}
