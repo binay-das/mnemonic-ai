bdeb2bf280d1ca8a0651e9304c80ca91d192bd9f
Binay <binaydas1123@gmail.com>
2026-01-23 00:20:53 +0530

    application providers

---
+++ app/providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { useRouter } from 'next/navigation';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+
+    return (
+        <SessionProvider>
+            <NextUIProvider navigate={router.push}>
+                <NextThemesProvider attribute="class" defaultTheme="system">
+                    {children}
+                </NextThemesProvider>
+            </NextUIProvider>
+        </SessionProvider>
+    );
+}
