ce79bf92366cc533d9762af5c1810a8a0aa86747
Binay <binaydas1123@gmail.com>
2026-04-29 10:13:28 +0530

    bug fix

--- proxy.ts
+++ proxy.ts
@@ -2,29 +2,31 @@ import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
-    function middleware(req) {
-        return NextResponse.next()
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        const path = req.nextUrl.pathname;
+
+        if (
+          path === "/" ||
+          path.startsWith("/auth/signin") ||
+          path.startsWith("/auth/signup")
+        ) {
+          return true;
+        }
+
+        return !!token;
+      }, 
     },
-    {
-        callbacks: {
-            authorized: ({ req, token }) => {
-                const path = req.nextUrl.pathname
-
-                if (path === '/') {
-                    return true
-                }
-
-                return !!token
-            },
-        },
-        pages: {
-            signIn: '/auth/signin',
-        },
-    }
-)
+    pages: {
+      signIn: "/auth/signin",
+    },
+  }
+);
 
 export const config = {
-    matcher: [
-        "/((?!api/auth|api/register|signin|signup|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$|.*\\.webp$).*)",
-    ]
-}
+  matcher: ["/((?!api|_next|favicon.ico).*)"],
+};
